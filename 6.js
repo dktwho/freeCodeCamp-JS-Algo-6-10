a3f337103a87f063aa021cd3d1b4e46f0eed3e63
Alexander Korobov <dktwho@mail.ru>
2023-06-13 10:41:34 +0300

    JS Algo 6

---
+++ 6.js
@@ -0,0 +1,10 @@
+function confirmEnding(str, target) {
+  var newStr = '';
+
+  newStr = str.substring(str.length - target.length);
+
+  return newStr === target;
+
+}
+
+confirmEnding("Bastian", "n");
